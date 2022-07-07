9eb54ac463ea24eff2724a348e50be8098c9b284
Branaverse <107011064+Branaverse@users.noreply.github.com>
2022-07-07 19:59:51 +0200

    Update SecuredFundsWallet.sol

--- SecuredFundsWallet.sol
+++ SecuredFundsWallet.sol
@@ -148,8 +148,8 @@ contract BranaSecuredFundsWallet {
     }
     function withdrawalBRANA(uint256 _amount, uint256 decimal, address to) external onlyOwner() {
         uint256 amount = BRANA.balanceOf(address(this)).sub(securedFundsVault);
-        require(amount > 0 && _amount <= amount, "No BRANA!");// can only withdraw what is not locked for Secured Funds.
         uint256 dcml = 10 ** decimal;
+        require(amount > 0 && _amount*dcml <= amount, "No BRANA!");// can only withdraw what is not locked for Secured Funds
         emit WithdrawalBRANA( _amount, decimal, to);
         BRANA.transfer(to, _amount*dcml);
     }
